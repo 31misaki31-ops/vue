395ee854c7dd1d5cb482d249e53802dbfab1e525
Misaki Mochiyama <3.1.misaki.3.1@gmail.com>
2026-01-21 21:34:15 +0900

    cat2とv-for2を追加

---
+++ SFC1.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { ref } from 'vue'
+const greeting = ref('Hello World!')
+</script>
+
+<template>
+  <p class="greeting">{{ greeting }}</p>
+</template>
+
+<style>
+.greeting {
+  color: skyblue;
+  font-weight: bold;
+}
+</style>
